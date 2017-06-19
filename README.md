a28de16e8172985a44da228a2504ed1b8187f06a
Davide Becker <hi@davide.me>
2017-06-19 23:11:07 +0200

    Set up first version of project structure

---
+++ README.md
@@ -0,0 +1,34 @@
+# EMotion
+
+## Development setup
+
+### Requirements
+[Node with npm](https://nodejs.org/en/)    
+[git](https://git-scm.com/)
+
+
+### First time setup
+1. Open your preferred command line and clone the repo using `git clone https://github.com/DavideBecker/p2-g6-EMotion.git`
+1. Use your favorite terminal to navigate to the project folder (Where this readme file is)
+1. If you never used gulp run `npm install gulp-cli -g`. This is required to run gulp commands from the terminal.
+1. Run `npm install` to install all dependencies. This can take a few minutes.
+1. Run `gulp` to build the source files. This will compile and compress JavaScript and CSS files as well as images
+1. Now  the project is set up. You can start it by using `./node_modules/.bin/electron .` or install electron globally by using `npm install electron -g` and just run `electron .`
+
+### Developing
+While developing you should run `gulp watch`. This will automatically compile files as they are saved as well as reload the electron instance (and a bunch of other cool stuff)
+
+
+## Project structure
+```javascript
+.
+├─── src
+│    ├─── images // All images in here get compressed
+│    ├─── scripts // Put .js files in this folder. Libraries etc in ./vendor
+│    │    ├─── vendor
+│    │    └─── main.js
+│    └─── styles
+│         └─── main.sass
+├─── index.html
+└─── main.js
+```
---
+++ gulpfile.js
@@ -0,0 +1,142 @@
+'use strict';
+
+var gulp            = require('gulp'),
+    path            = require('path'),
+    colors          = require('colors'),
+    sequence        = require('run-sequence'),
+    gutil           = require('gulp-util'),
+    clean           = require('gulp-clean'),
+
+    eslint          = require('gulp-eslint'),
+
+    sass            = require('gulp-sass'),
+    cssnano         = require('gulp-cssnano'),
+    sourcemaps      = require('gulp-sourcemaps'),
+    autoprefixer    = require('gulp-autoprefixer'),
+
+    uglify          = require('gulp-uglify'),
+    concat          = require('gulp-concat'),
+    // include         = require('gulp-include'),
+
+    imagemin        = require('gulp-imagemin')
+
+colors.setTheme({
+    silly: 'rainbow',
+    input: 'grey',
+    verbose: 'cyan',
+    prompt: 'grey',
+    info: 'green',
+    data: 'grey',
+    help: 'cyan',
+    warn: 'yellow',
+    debug: 'blue',
+    error: 'red'
+});
+
+var eslintErrorSeverity = ['none', 'Warning'.warn, 'FATAL'.error];
+
+var baseUrl = './';
+var distUrl = 'assets/';
+var vendorFolder = 'vendor/';
+
+var src = {
+    'css': baseUrl + 'src/styles/',
+    'js': baseUrl + 'src/scripts/',
+    'img': baseUrl + 'src/images/**',
+
+    'jsMain': 'main.js',
+    'cssMain': 'main.sass'
+}
+
+var dist = {
+    'css': baseUrl + distUrl + 'css/',
+    'js': baseUrl + distUrl + 'js/',
+    'img': baseUrl + distUrl + 'img/',
+}
+
+function fixedLength(str, len) {
+    return str + Array(len).join(' ').slice(-len + str.length);
+}
+
+gulp.task('styles', function() {
+    gulp.src(src.css + src.cssMain)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(cssnano())
+        .pipe(sourcemaps.write('./'))
+
+        .pipe(gulp.dest(dist.css))
+
+    gulp.src(src.css + vendorFolder + '/**').pipe(gulp.dest(dist.css + vendorFolder));
+})
+
+gulp.task('scripts:lint', function() {
+    gulp.src(src.js + '*.js')
+        .pipe(eslint({
+            fix: false,
+            useEslintrc: true
+        }))
+        .pipe(eslint.result((result) => {
+            if(result.messages.length) {
+                console.log('');
+                console.log('    ESLint result for', result.filePath);
+                console.log('    # Warnings: ', result.warningCount);
+                console.log('    # Errors:   ', result.errorCount);
+                result.messages.forEach(function(err) {
+                    console.log(
+                        '    ' +
+                        fixedLength(err.ruleId ? err.ruleId : '', 20).info +
+                        fixedLength('Line ' + err.line + ':' + err.column, 16).verbose +
+                        fixedLength(eslintErrorSeverity[err.severity], 10) + ' ' + err.message);
+                });
+            }
+        }))
+})
+
+gulp.task('scripts', ['scripts:lint'], function() {
+    gulp.src([src.js + '*.js', src.js + src.jsMain])
+        .pipe(sourcemaps.init())
+        .pipe(concat(src.jsMain))
+        // .pipe(include({
+        //     includePaths: [
+        //         path.join(__dirname, 'src', 'scripts'),
+        //     ]
+        // }))
+            // .on('error', console.log)
+        .pipe(gulp.dest(dist.js))
+        //.pipe(sourcemaps.write('./'))
+        .pipe(uglify().on('error', function(err) {
+            console.log('\n    ' + err.toString().replace(new RegExp('\n', 'g'), '\n    '));
+        }))
+        .pipe(gulp.dest(dist.js));
+
+    gulp.src(src.js + vendorFolder + '/**').pipe(gulp.dest(dist.js + vendorFolder));
+});
+
+gulp.task('images', function() {
+    gulp.src(src.img)
+        .pipe(imagemin({
+            progressive: true
+        }))
+
+        .pipe(gulp.dest(dist.img))
+});
+
+//Images aren't included here as even a few can easily impact performance
+gulp.task('watch', function() {
+    gulp.watch(src.css + '**', ['styles']);
+
+    gulp.watch(src.js + '**', ['scripts']);
+
+    gulp.watch(src.sprites, ['sprites']);
+})
+
+gulp.task('clean', function() {
+    return gulp.src(baseUrl + distUrl)
+        .pipe(clean());
+})
+
+gulp.task('default', function() {
+    sequence('clean', ['styles', 'scripts', 'images']);
+})
