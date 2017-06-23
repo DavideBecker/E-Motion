bd8305f6beee9bb457b8a216aa82f8a5dcd98618
Davide Becker <davide.becker@stud.h-da.de>
2017-06-23 23:06:56 +0200

    Updated calculations - Added calculation for how many trucks we need to cover all cars - Expanded the time unit conversion function - Improved some minor layout stuff

--- prototypes/calculations.js
+++ prototypes/calculations.js
@@ -13,9 +13,20 @@ var config = {
         chargeSpeed: 22, // kWh
         mileage: 15 // kWh per 100 KM
     },
+
+    // Static
+    // averageDistanceBetweenCharges: 800, // M
+    // averageDistanceToLocation:  25, // KM
+    // averageTruckUptime: 7, // Hours
+    // averageCarsToChargePerDay: 1000, // Amount of cars
+    // chargePercentage: 0.25, //decimal percentage (0.8 as 80%)
+    // deployedTrucks: 25, // Amount of trucks
+
+    // Random
     averageDistanceBetweenCharges: random(500, 2000), // M
     averageDistanceToLocation:  random(20, 40), // KM
     averageTruckUptime: random(3, 8), // Hours
+    averageCarsToChargePerDay: random(250, 3000), // Amount of cars
     chargePercentage: 0.25, //decimal percentage (0.8 as 80%)
     deployedTrucks: random(5, 50), // Amount of trucks
 }
@@ -40,11 +51,17 @@ function convertTime(time, from, to) {
     if(from == 'i') { secs = time * 60 }
     if(from == 'h') { secs = time * 60 * 60 }
     if(from == 'd') { secs = time * 60 * 60 * 24 }
+    if(from == 'w') { secs = time * 60 * 60 * 24 * 7 }
+    if(from == 'm') { secs = time * 60 * 60 * 24 * 30.4375 }
+    if(from == 'y') { secs = time * 60 * 60 * 24 * 365.25}
 
     if(to == 's') { return secs }
     if(to == 'i') { return secs / 60 }
     if(to == 'h') { return secs / 60 / 60 }
     if(to == 'd') { return secs / 60 / 60 / 24 }
+    if(to == 'w') { return secs / 60 / 60 / 24 / 7 }
+    if(to == 'm') { return secs / 60 / 60 / 24 / 30.4375 }
+    if(to == 'y') { return secs / 60 / 60 / 24 / 365.25 }
 
     return false;
 }
@@ -60,18 +77,14 @@ function prettyPrintConfig() {
     console.log('      Capacity:              ', config.car.capacity, 'kWh');
     console.log('      Charging speed:        ', config.car.chargeSpeed, 'kWh');
     console.log('      Mileage:               ', config.car.mileage, 'kWh per 100 KM');
-    console.log('   VALUES');
-    console.log('      Average distance a truck has to travel between charges:   ', config.averageDistanceBetweenCharges, 'M');
-    console.log('      Average distance between locations (villages etc):        ', config.averageDistanceToLocation, 'KM');
-    console.log('      Average time a truck is driving around and charging cars: ', config.averageTruckUptime, 'hours');
+    console.log('   STATIC');
     console.log('      Percentage of the capacity a truck charges each car:      ', config.chargePercentage * 100, '%');
+    console.log('   RANDOMIZED VALUES');
+    console.log('      Average distance a truck has to travel between charges:   ', config.averageDistanceBetweenCharges, 'm');
+    console.log('      Average distance between locations (villages etc):        ', config.averageDistanceToLocation, 'km');
+    console.log('      Average time a truck is driving around and charging cars: ', config.averageTruckUptime, 'hours');
+    console.log('      Average amount of cars that need to be charged each day:  ', config.averageCarsToChargePerDay, 'cars');
     console.log('      Amount of trucks that are deployed:                       ', config.deployedTrucks, 'trucks');
-
-
-    // averageDistanceBetweenCharges: 1000, // M
-    // averageDistanceToLocation:  20, // KM
-    // chargePercentage: 0.25 //decimal percentage (0.8 as 80%)
-
 }
 
 function fineRound(number) {
@@ -83,7 +96,7 @@ function random(min, max) {
 }
 
 
-// Calculates the mileage per KM
+// Calculate the mileage per KM
 // mileage in kWh per 100 KM
 // returns kWh per KM
 function calculateMileagePerKilometre(mileage) {
@@ -158,6 +171,14 @@ function calculateChargeableCarsInTimeframe(averageChargeTime, availableTime) {
     return Math.floor(availableTime / averageChargeTime * 10) / 10
 }
 
+// Calculate how many trucks are needed to charge a specified amount of cars in a given timeframe
+// chargeableCarsInTimeframe in amount is the result of calculateChargeableCarsInTimeframe()
+// coverageRequirement in amount of cars that need to be charged
+// returns amount of trucks needed to charge the cars
+function calculateTrucksNeededForCoverage(chargeableCarsInTimeframe, coverageRequirement) {
+    return coverageRequirement / chargeableCarsInTimeframe
+}
+
 
 // Test output
 var chargingCapacity = calculateChargingCapacity(config.emotion.capacity, config.emotion.mileage, config.averageDistanceToLocation)
@@ -184,14 +205,12 @@ console.log();
 console.log('RAW VALUES')
 
 console.log('chargingCapacity', chargingCapacity, 'kWh');
-
 console.log('amountOfCarsThatCanBeCharged', amountOfCarsThatCanBeCharged);
 console.log('travelTime', prettyTime(travelTime), travelTime);
 console.log('chargeTime', prettyTime(chargeTime), chargeTime);
-
 console.log('chargeCycleTime', prettyTime(chargeCycleTime), chargeCycleTime);
-
 console.log('averageChargeTime', prettyTime(averageChargeTime), averageChargeTime);
+console.log('carsChargedPerTruck', carsChargedPerTruck, carsChargedPerTruck);
 
 console.log()
 
@@ -203,5 +222,6 @@ console.log('In', config.averageTruckUptime, 'hours a truck can charge', carsCha
 console.log('All', config.deployedTrucks, 'trucks can charge', carsChargedPerDay, 'cars per day');
 console.log('That\'s', fineRound(carsChargedPerMonth / config.deployedTrucks), 'cars per month, or', fineRound(carsChargedPerYear / config.deployedTrucks), 'cars per year which a single truck can charge')
 console.log('Or in total for', config.deployedTrucks, 'trucks,', carsChargedPerMonth, 'cars per month, or', carsChargedPerYear, 'cars per year');
+console.log('In order to charge', config.averageCarsToChargePerDay, 'cars in', config.averageTruckUptime, 'hours it would require', Math.ceil(calculateTrucksNeededForCoverage(carsChargedPerTruck, config.averageCarsToChargePerDay)), 'trucks')
 
 console.log();
