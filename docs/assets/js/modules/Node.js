377d3980270c104d40ad268b3c6abbfb3fb2a774
Davide Becker <davide.becker@stud.h-da.de>
2017-07-14 01:14:14 +0200

    Updated GitHub Pages

--- docs/assets/js/modules/Node.js
+++ docs/assets/js/modules/Node.js
@@ -1,15 +1,2 @@
-'use strict';
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Node = function Node(raw) {
-    _classCallCheck(this, Node);
-
-    this.connectedTo = raw.connectedTo;
-    this.id = raw.id;
-    this.isCity = raw.isCity;
-    this.x = raw.x;
-    this.y = raw.y;
-    this.type = 'node';
-};
+"use strict";function _classCallCheck(t,i){if(!(t instanceof i))throw new TypeError("Cannot call a class as a function")}var Node=function t(i){_classCallCheck(this,t),this.connectedTo=i.connectedTo,this.id=i.id,this.isCity=i.isCity,this.x=i.x,this.y=i.y,this.type="node"};
 //# sourceMappingURL=Node.js.map
